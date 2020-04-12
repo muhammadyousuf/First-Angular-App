48fd4f0b1b42eb537fcbe1e598c7b96d9b81d4f8
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-04-12 22:34:57 +0500

    change component style and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,5 +4,12 @@ import { Component } from "@angular/core";
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: green;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {}
