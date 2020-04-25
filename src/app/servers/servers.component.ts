dbda2abb90cd77e01dabe3a8ef19bdd11aa3f806
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-04-26 00:11:35 +0500

    single way string data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,9 +4,7 @@ import { Component, OnInit } from "@angular/core";
   // selector: "[app-servers]",
   // selector: ".app-servers",
   selector: "app-servers",
-  template: ` <app-server></app-server>
-
-    <app-server></app-server>`,
+  template: ` <app-server></app-server>`,
   styleUrls: ["./servers.component.css"],
 })
 export class ServersComponent implements OnInit {
