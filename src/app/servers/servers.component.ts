1d1d08b192ac10fc312fc7d619c8bcf36610e1f4
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-04-26 00:48:11 +0500

    input field data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ import { Component, OnInit } from "@angular/core";
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStaus = "No server was created!'";
+  serverName = "";
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -21,5 +22,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStaus = "Server was created!'";
+  }
+
+  onUpdateServerName(event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
