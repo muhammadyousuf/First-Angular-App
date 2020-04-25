f3e29198a3ac01db67a1c8ee7553e2cb6dbb9077
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-04-26 00:59:20 +0500

    two way data binding input form

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from "@angular/core";
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStaus = "No server was created!'";
-  serverName = "";
+  serverName = "Tested";
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
